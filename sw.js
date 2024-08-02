aab3ff1ad5b085a3a09f01067e3f1b90d668939f
Liam Bruna <135740615+LiamBruna@users.noreply.github.com>
2024-08-01 23:24:48 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icon.png',
+        '/_expo/static/js/web/AppEntry-b5b05a17cb04052b3e5c2a3d92d66da7.js'
+      ]);
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        })
-    );
-  });
-  
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
