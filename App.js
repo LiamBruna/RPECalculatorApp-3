97ac64d15ccda50a339f18418c77c3dc3a92f3bb
Ven0s00 <135740615+Ven0s00@users.noreply.github.com>
2024-08-01 20:18:02 -0400

    Inicio

--- App.js
+++ App.js
@@ -1,89 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Button, View, StyleSheet } from 'react-native';
+import RPECalculator from './components/RPECalculator'; // Tu aplicación actual
+import Timer from './components/Timer'; // El componente del cronómetro
 
 export default function App() {
-  const [weight, setWeight] = useState('');
-  const [reps, setReps] = useState('');
-  const [rpe, setRpe] = useState('');
-  const [e1rm, setE1rm] = useState('');
-  const [repsWant, setRepsWant] = useState('');
-  const [rpeWant, setRpeWant] = useState('');
-  const [weightWant, setWeightWant] = useState('');
-
-  useEffect(() => {
-    const result = calcularE1rm(weight, reps, rpe);
-    if (result) {
-      setE1rm(result.e1rm.toString());
-      
-    }
-  }, [weight, reps, rpe]);
-
-  useEffect(() => {
-    const result = pesoObjetivo(e1rm, repsWant, rpeWant);
-    if (result) {
-      setWeightWant(result.pesoObjetivo.toString());
-    }
-  }, [e1rm, repsWant, rpeWant])  
+  const [showComponent, setShowComponent] = useState('');
 
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <Text style={styles.title}>Top Set</Text>
-        <Text style={styles.subtitle}>Weight</Text>
-        <TextInput style={styles.input} value={weight} onChangeText={setWeight} />
-        <Text style={styles.subtitle}>REPS</Text>
-        <TextInput style={styles.input} value={reps} onChangeText={setReps} />
-        <Text style={styles.subtitle}>RPE</Text>
-        <TextInput style={styles.input} value={rpe} onChangeText={setRpe} />
-        <Text style={styles.subtitle}>E1RM</Text>
-        <TextInput style={styles.e1rm} value={e1rm} onChangeText={setE1rm} />
-      </View>
-      <View style={styles.card}>
-        <Text style={styles.title}>Back Off</Text>
-        <Text style={styles.subtitle}>REPS</Text>
-        <TextInput style={styles.input} value={repsWant} onChangeText={setRepsWant} />
-        <Text style={styles.subtitle}>RPE</Text>
-        <TextInput style={styles.input} value={rpeWant} onChangeText={setRpeWant} />
-        <Text style={styles.subtitle}>PESO</Text>
-        <TextInput style={styles.backoff} value={weightWant} onChangeText={setWeightWant} />
-      </View>
+      {showComponent === 'RPECalculator' && <RPECalculator setShowComponent={setShowComponent} />}
+      {showComponent === 'Timer' && <Timer setShowComponent={setShowComponent}/>}
+      {showComponent === '' && (
+        <View style={styles.card}>
+          <Button title="RPECalculator" onPress={() => setShowComponent('RPECalculator')} color="#2c2a2a" />
+        </View>
+      )}
+      {showComponent === '' && (
+        <View style={styles.card}>
+          <Button title="Cronómetro" onPress={() => setShowComponent('Timer')} color="#2c2a2a" />
+        </View>
+      )}
     </View>
   );
-}
-
-function calcularPorcentajeEsfuerzo(reps, rpe) {
-  if (rpe > 10) rpe = 10.0;
-  if (reps < 1 || rpe < 4) return 0.0;
-  if (reps === 1 && rpe === 10.0) return 100.0;
-  const esfuerzo = (10.0 - rpe) + (reps - 1);
-  if (esfuerzo >= 16) return 0.0;
-  const puntoInterseccion = 2.92;
-  if (esfuerzo <= puntoInterseccion) {
-    const coeficienteCuadratico = 0.347619;
-    const coeficienteLineal = -4.60714;
-    const terminoIndependiente = 99.9667;
-    return coeficienteCuadratico * esfuerzo ** 2 + coeficienteLineal * esfuerzo + terminoIndependiente;
-  } else {
-    const pendiente = -2.64249;
-    const ordenadaOrigen = 97.0955;
-    return pendiente * esfuerzo + ordenadaOrigen;
-  }
-}
-
-function calcularE1rm(pesoActual, repsActual, rpeActual) {
-  const porcentajeEsfuerzoActual = calcularPorcentajeEsfuerzo(repsActual, rpeActual);
-  if (porcentajeEsfuerzoActual <= 0) return null;
-  const e1rm = Math.round((pesoActual / porcentajeEsfuerzoActual * 100) * 10) / 10;
-  if (e1rm <= 0) return null;
-  else return { e1rm };
-}
-
-function pesoObjetivo(e1rm, repsObjetivo, rpeObjetivo) {
-  const porcentajeEsfuerzoObjetivo = calcularPorcentajeEsfuerzo(repsObjetivo, rpeObjetivo);
-  if (porcentajeEsfuerzoObjetivo <= 0) return null;
-  const pesoObjetivo = Math.round((e1rm / 100 * porcentajeEsfuerzoObjetivo) * 10) / 10;
-  return { pesoObjetivo };
 }
 
 const styles = StyleSheet.create({
@@ -91,39 +29,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'grey'
+    backgroundColor: '#361111'
   },
   card: {
     width: '80%',
-    borderWidth: 1,
+    borderWidth: 3,
     borderRadius: 10,
     padding: 20,
     marginBottom: 20,
-    backgroundColor: 'darkgrey'
+    backgroundColor: '#2c2a2a'
   },
-  title: {
-    fontSize: 30,
-    marginBottom: 10,
-    color: 'white'
-  },
-  subtitle: {
-    fontSize: 20,
-    color: 'white'
-  },
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 5,
-    marginBottom: 10,
-    color: 'white'
-  },
-  e1rm: {
-    fontSize: 18,
-    color: 'white'
-  },
-  backoff: {
-    fontSize: 18,
-    color: 'white'
-  }
 });
---
+++ components/Timer.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, TextInput, StyleSheet } from 'react-native';
+
+export default function Timer({ setShowComponent }) {
+  const [minutes, setMinutes] = useState('00');
+  const [seconds, setSeconds] = useState('00');
+  const [isActive, setIsActive] = useState(false);
+  const [countdown, setCountdown] = useState(0);
+
+  function toggle() {
+    setIsActive(!isActive);
+    setCountdown(parseInt(minutes) * 60 + parseInt(seconds));
+  }
+
+  function reset() {
+    setMinutes('00');
+    setSeconds('00');
+    setCountdown(0)
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive && countdown > 0) {
+      interval = setInterval(() => {
+        setCountdown(countdown => countdown - 1);
+      }, 1000);
+    } else if (!isActive && countdown !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, countdown]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{Math.floor(countdown / 60)}m {countdown % 60}s</Text>
+      <TextInput style={styles.input} value={minutes} onChangeText={setMinutes} keyboardType="numeric" placeholder="Minutos"/>
+      <TextInput style={styles.input} value={seconds} onChangeText={setSeconds} keyboardType="numeric" placeholder="Segundos"/>
+      <View style={styles.card}>
+        <Button title={isActive ? 'Pause' : 'Start'} onPress={toggle} color="#2c2a2a"/>
+      </View>
+      <View style={styles.card}>
+        <Button title="Reset" onPress={reset} color="#2c2a2a"/>
+      </View>
+      <View style={styles.card}>
+        <Button title="Volver al inicio" onPress={() => setShowComponent('')} color="#2c2a2a"/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#361111'
+  },
+  input: {
+    fontSize: 40,
+    borderWidth: 3,
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 10,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  text: {
+    fontSize: 50,
+    padding: 5,
+    marginBottom: 10,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  card: {
+    height: '8%',
+    width: '60%',
+    borderWidth: 3,
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 20,
+    backgroundColor: '#2c2a2a',
+    fontWeight: 'bold'
+  },
+});
