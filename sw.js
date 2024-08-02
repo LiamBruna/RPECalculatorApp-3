36b8cf7c844fee20a2a6b7f7ca5037b647617b26
Ven0s00 <135740615+Ven0s00@users.noreply.github.com>
2024-08-02 00:41:59 -0400

    sw.js actualizado

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
-        '/',
+        './',
         './index.html',
         './icon.png',
         './_expo/static/js/web/AppEntry-b5b05a17cb04052b3e5c2a3d92d66da7.js'
